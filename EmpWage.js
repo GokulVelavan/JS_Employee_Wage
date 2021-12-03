2e0065db188bb6d7d149f82216c5d8924d35539f
GokulVelavan <gokulvelavans@gmail.com>
2021-12-03 12:45:48 +0530

    UC1-Employee Attendance

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,13 @@
-console.log("EmpWage using JavaScript");
+const IS_Present = 1;
+
+let empInput = Math.floor(Math.random()*2);
+
+if (IS_Present == empInput)
+{
+    
+    Console.log("Employee is Present");
+}
+else {
+    
+    Console.log("Employee is Absent");
+}
