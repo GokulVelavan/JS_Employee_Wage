f76902c386ca2d6fe3e8700a3276ab0a1a93966f
GokulVelavan <gokulvelavans@gmail.com>
2021-12-06 07:45:59 +0530

    emp wage for employee

--- EmpWage.js
+++ EmpWage.js
@@ -1,13 +1,26 @@
-const IS_Present = 1;
+const IS_FullTime = 1;
+const IS_PartTime = 2;
+const PART_TIME_HOURS= 4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+let empHrs= 0;
+let empCheck = Math.floor(Math.random()*10)%3;
 
-let empInput = Math.floor(Math.random()*2);
+switch (empCheck)
+            {
+                case IS_PartTime:
+                    empHrs = PART_TIME_HOURS;
+                    break;
 
-if (IS_Present == empInput)
-{
-    
-    Console.log("Employee is Present");
-}
-else {
-    
-    Console.log("Employee is Absent");
-}
+                case IS_FullTime:
+                    empHrs = FULL_TIME_HOURS;
+                    break;
+
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+     
+            let empWage = WAGE_PER_HOUR * empHrs;
+             console.log("the Employee age is "+ empWage);
