3ade8e5b296e5de4a2b389d761b53876e0a40ce9
GokulVelavan <gokulvelavans@gmail.com>
2021-12-06 19:12:25 +0530

    Array Operations

--- EmpWage.js
+++ EmpWage.js
@@ -39,3 +39,77 @@ function getWorkingHours(empCheck)
                     }
                     let empWage =calDailyWage(totalEmpHrs);
                   console.log("total days ",totalWorkingDays,"total hrs",totalEmpHrs, "the Employee wage is "+ empWage);
+
+
+
+
+                  let totEmpWage=0;
+                  function sum(dailywage){
+                      totEmpWage+=dailywage;
+                  }
+                  empDailyWageArr.forEach(sum);
+                  console.log("UC7A-total days ",totalWorkingDays,"total hrs",totalEmpHrs, "the Employee wage is "+ totEmpWage);
+
+                
+                  function totalWages(totalwage,dailywage)
+                  {
+                      return totalwage + dailywage;
+                  }
+               
+                  console.log("UC7A -Wage with reduce",empDailyWageArr.reduce(totalWages,0));
+                  
+
+                  let dailyCntr=0;
+                  function mapDayWithWage(dailywage)
+                  {
+                      dailyCntr++;
+                      return dailyCntr + "= "+dailywage;
+                  }
+                  let mapDayWithWageArr=empDailyWageArr.map(mapDayWithWage);
+                  console.log("UC7B-Daily Wage Map");
+                  console.log(mapDayWithWageArr);
+
+
+                  function fulltimeWage(dailywage)
+                  {
+                      return dailywage.includes("160");
+                  }
+                  let fullDayWageArr=mapDayWithWageArr.filter(fulltimeWage);
+                  console.log("UC7C -Daily Wage FILTER when FULLtime wage Earned");
+                  console.log(fullDayWageArr);
+
+
+                  //UC 7D
+
+                  function findFulltimeWAGE(dailyWage)
+                  {
+                      return dailyWage.includes("160");
+                  }
+                  console.log("UC7D -First Time  FULLtime wage Earned",mapDayWithWageArr.find(findFulltimeWAGE));
+
+                  //UC-7E
+
+                  function isAllFulltimeWage(dailyWage)
+                  {
+                      return dailyWage.includes("160");
+                  }
+                  console.log("UC7E -Check all Element has full time wage",fullDayWageArr.every(isAllFulltimeWage));
+               
+
+                  //UC-7F
+                  function isAnyPartTimeWage(dailyWage)
+                  {
+                      return dailyWage.includes("160");
+                  }
+                  console.log("UC7E -Check if Any Element has part time wage",mapDayWithWageArr.some(isAnyPartTimeWage));
+               
+
+                  //UC7G 
+                  function totalDaysWorked(numOfDays,dailyWage)
+                  {
+                      if(dailyWage>0) return numOfDays+1;
+                      return numOfDays;
+                  }
+                  console.log("UC-7G Number of Days Emp Worked",empDailyWageArr.reduce(totalDaysWorked,0));
+
+           
