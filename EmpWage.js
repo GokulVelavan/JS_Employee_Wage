f5924bd10ee7f6260fbb58a547e9c70f29acd411
GokulVelavan <gokulvelavans@gmail.com>
2021-12-06 08:38:54 +0530

    program in functions

--- EmpWage.js
+++ EmpWage.js
@@ -3,24 +3,23 @@ const IS_PartTime = 2;
 const PART_TIME_HOURS= 4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-let empHrs= 0;
-let empCheck = Math.floor(Math.random()*10)%3;
 
-switch (empCheck)
-            {
-                case IS_PartTime:
-                    empHrs = PART_TIME_HOURS;
-                    break;
-
-                case IS_FullTime:
-                    empHrs = FULL_TIME_HOURS;
-                    break;
-
-
-                default:
-                    empHrs = 0;
-                    break;
-            }
-     
-            let empWage = WAGE_PER_HOUR * empHrs;
-             console.log("the Employee age is "+ empWage);
+function getWorkingHours(empCheck)
+{
+    
+    switch (empCheck)
+    {
+        case IS_PartTime:
+            return PART_TIME_HOURS;
+        case IS_FullTime:
+            return FULL_TIME_HOURS;
+         default:
+            return 0;
+      }
+}
+                    
+                    let empHrs= 0;
+                    let empCheck = Math.floor(Math.random()*10)%3;
+                    empHrs=getWorkingHours(empCheck);
+                    let empWage = WAGE_PER_HOUR * empHrs;
+             console.log("the Employee wage is "+ empWage);
